86845eecc9c629fc901b8524099e4cfda1829ab9
Fabián Machuca <20019496+Gfabianmm@users.noreply.github.com>
2023-10-20 04:23:07 -0500

    avance

--- TestNagSAApp/Domain/Factura/FacturaDTO.cs
+++ TestNagSAApp/Domain/Factura/FacturaDTO.cs
@@ -5,6 +5,10 @@ namespace TestNagSAApp.Domain.Factura
 {
     public class FacturaDTO
     {
+        public FacturaDTO() {
+            FacturaDetalle = new List<FacturaDetalleDTO>();
+            FacturaFormaPago= new List<FacturaFormaPagoDTO>();
+        }
         public Guid? IdFactura { get; set; }
 
         [Required, StringLength(500)]
